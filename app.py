1214461fe34d4618682b7fe4f23c069ea14011b5
MKim <mrn.kim07@gmail.com>
2023-04-19 17:38:30 -0700

    finish questions.html results.html

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def get_home():
-    
-    return render_template('questions.html')
+    prompts = silly_story.prompts
+    print(silly_story.prompts)
+    # html = render_template('questions.html')
+    return render_template("questions.html", prompts=prompts)
+
+
+# grab words from the silly_story class -> save it in a variable
+
+# loop over the list
+
+# inject each element to questions.html template as input in the label
+
+@app.get("/results")
+def get_result():
+    story = silly_story.get_result_text(request.args)
+
+    # print("request.ars", request.args)
+    return render_template("results.html",story=story )
+# gather the information from the form
